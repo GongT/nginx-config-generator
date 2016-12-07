4c71bb62d4468816dd96932c27d717b05e55c02c
GongT <gongteng524702837@gmail.com>
2016-12-07 18:18:26 +0800

    add debug

--- src/service_template/structure/ssl.ts
+++ src/service_template/structure/ssl.ts
@@ -1,5 +1,6 @@
-import {existsSync, mkdirSync, writeFileSync} from "fs";
+import {existsSync, writeFileSync} from "fs";
 import {resolve} from "path";
+import {sync as mkdirpSync} from "mkdirp";
 
 const certbotEnabled = existsSync('/data/certbot/');
 
@@ -7,31 +8,30 @@ export function createSSL(arg, missingSafe: boolean) {
 	const {service, configMainBody, configFileServer} = arg;
 	
 	if (certbotEnabled) {
-		const exampleFolder = resolve('/data/certbot/', service.outerDomainName);
-		if (!existsSync(resolve(exampleFolder, 'index.html'))) {
-			if (!existsSync(exampleFolder)) {
-				mkdirSync(exampleFolder);
-			}
-			writeFileSync(resolve(exampleFolder, 'index.html'), `<!DOCTYPE html>
+		const exampleFolder = resolve('/data/certbot/', service.serverName, '.well-known');
+		if (!existsSync(exampleFolder)) {
+			mkdirpSync(exampleFolder);
+		}
+		writeFileSync(resolve(exampleFolder, 'index.html'), `<!DOCTYPE html>
 <html>
 <head><title>Example page</title></head>
 <body>
 <h1>This path is for certbot ssl request.</h1>
 </body>
 </html>`);
-		}
 	}
 	
-	if (!existsSync(resolve('/etc/letsencrypt/live', service.outerDomainName, 'privkey.pem'))) {
+	const certFile = resolve('/etc/letsencrypt/live', service.outerDomainName, 'privkey.pem');
+	if (!existsSync(certFile)) {
 		if (missingSafe) {
 			return `
-### no ssl cert found...
+### no ssl cert found...  at: ${certFile}
 listen 443;
 listen [::]:443;
 `;
 		} else {
 			return `
-### no ssl cert found...
+### no ssl cert found...  at: ${certFile}
 `;
 		}
 	}
