56db1861296cc80210a45fd7d6d8c58e13723ac6
GongT <gongteng524702837@gmail.com>
2016-12-07 18:33:16 +0800

    update ssl config

--- src/service_template/structure/ssl.ts
+++ src/service_template/structure/ssl.ts
@@ -43,10 +43,15 @@ listen [::]:443 ssl http2;
 ### location.ssl
 ssl_certificate /etc/letsencrypt/live/${service.outerDomainName}/fullchain.pem;
 ssl_certificate_key /etc/letsencrypt/live/${service.outerDomainName}/privkey.pem;
-include /etc/letsencrypt/options-ssl-nginx.conf;
 ssl_trusted_certificate /etc/letsencrypt/live/${service.outerDomainName}/chain.pem;
 ssl_stapling on;
 ssl_stapling_verify on;
+ssl_session_cache shared:SSL:1m;
+ssl_session_timeout 1440m;
+ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+ssl_prefer_server_ciphers on;
+# Using list of ciphers from "Bulletproof SSL and TLS"
+ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256 ECDHE-ECDSA-AES256-GCM-SHA384 ECDHE-ECDSA-AES128-SHA ECDHE-ECDSA-AES256-SHA ECDHE-ECDSA-AES128-SHA256 ECDHE-ECDSA-AES256-SHA384 ECDHE-RSA-AES128-GCM-SHA256 ECDHE-RSA-AES256-GCM-SHA384 ECDHE-RSA-AES128-SHA ECDHE-RSA-AES128-SHA256 ECDHE-RSA-AES256-SHA384 DHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 DHE-RSA-AES128-SHA DHE-RSA-AES256-SHA DHE-RSA-AES128-SHA256 DHE-RSA-AES256-SHA256 EDH-RSA-DES-CBC3-SHA";
 ## createSSL END
 `
 }
