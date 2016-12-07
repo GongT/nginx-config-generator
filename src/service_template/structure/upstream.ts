a5716bcdd4795cd558d011084202f58eaf707133
GongT <gongteng524702837@gmail.com>
2016-12-07 15:42:38 +0800

    fix self detect

--- src/service_template/structure/upstream.ts
+++ src/service_template/structure/upstream.ts
@@ -1,13 +1,13 @@
 import {IServiceConfig} from "../../handler";
 import {whoAmI} from "../../../who_am_i/index";
 export function createUpstream(service: IServiceConfig) {
-	const externals = service.machines.map((server) => {
+	const externals = service.machines.filter((server) => {
 		return server !== whoAmI.id;
 	});
 	let selfLine = '# no self';
 	if (service.existsCurrentServer) {
 		selfLine = `server ${service.serviceName} weight=100 fail_timeout=1s; # <- self`;
-	} else if (externals.length === 0 && !service.existsCurrentServer) {
+	} else if (externals.length === 0) {
 		selfLine = `server upstream:8888 weight=100 fail_timeout=1s; # TRIGGER ERROR`;
 	}
 	
