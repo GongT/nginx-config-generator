3fdbab020c570aadf1906d0aaff908f02ef82bc2
GongT <gongteng524702837@gmail.com>
2017-04-06 16:08:16 +0800

    fix no server if only internet server available

--- src/service_template/structure/upstream.ts
+++ src/service_template/structure/upstream.ts
@@ -7,24 +7,22 @@ export function createUpstream(service: IServiceConfig) {
 	
 	const localPriority = service.existsCurrentServer? 'backup' : '';
 	const unique: any = {};
+	let anyLocalServer = !!service.existsCurrentServer;
 	
 	service.machines.forEach((serverName) => {
 		if (serverName === whoAmI.id) {
-			return false;
+			return; // the current server
 		}
 		const server = serverMap[serverName];
 		if (server.network === whoAmI.network) {
 			debugFn(`  local network: ${server.internal}`);
-			if (!unique[server.internal]) {
-				unique[server.internal] = true;
-				ret.push(`\tserver ${server.internal} weight=100 ${localPriority} fail_timeout=1s; # local network`);
-			}
+			unique[server.internal] = `server ${server.internal} weight=100 ${localPriority} fail_timeout=1s; # local network`;
+			anyLocalServer = anyLocalServer || (localPriority !== 'backup');
 		} else {
 			const host = server.external || server.interface;
 			debugFn(`  remote network: ${host}`);
 			if (!unique[host]) {
-				unique[host] = true;
-				ret.push(`\tserver ${host} weight=1 backup fail_timeout=10s; # internet`);
+				unique[host] = `server ${host} weight=1 backup fail_timeout=10s; # internet`;
 			}
 		}
 	});
@@ -32,10 +30,20 @@ export function createUpstream(service: IServiceConfig) {
 	if (service.existsCurrentServer) {
 		debugFn(`  self: docker - ${service.existsCurrentServer}`);
 		ret.push(`\tserver ${service.existsCurrentServer} weight=100 fail_timeout=1s; # server in docker`);
+		anyLocalServer = true;
 	} else if (0 === Object.keys(unique).length) { // if no remote server exists
 		debugFn(`  Error: no any server available!`);
 		ret.push(`\tserver 127.0.0.1:8888 fail_timeout=1s; # TRIGGER ERROR`);
 	}
+	
+	if (!anyLocalServer) { // all server is internet, so each line have a "backup"
+		Object.keys(unique).forEach((index) => {
+			unique[index] = unique[index].replace(/ backup /, ' ');
+		});
+	}
+	Object.values(unique).forEach((value) => {
+		ret.push(`\t${value}`);
+	});
 	
 	ret.push('}');
 	
