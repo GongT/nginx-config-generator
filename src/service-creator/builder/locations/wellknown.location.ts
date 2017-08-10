61af0c200ee3cc5fd43d9cace3ee1d063c022cb4
GongT <admin@gongt.me>
2017-08-10 19:34:25 +0800

    fix certbot path

--- src/service-creator/builder/locations/wellknown.location.ts
+++ src/service-creator/builder/locations/wellknown.location.ts
@@ -1,5 +1,5 @@
 import {writeFileSync} from "fs";
-import {ensureDirSync, pathExistsSync} from "fs-extra";
+import {ensureDirSync} from "fs-extra";
 import {ILocationConfig, RouteDirection} from "../../config.define";
 import {AccessLog} from "../../global.http";
 import {LocationConfigFile} from "../../template/location-configfile";
@@ -14,19 +14,17 @@ export class WellknownLocationConfigFile extends LocationConfigFile {
 const isDocker = !!process.env.RUN_IN_DOCKER;
 
 export class WellknownLocation extends LocationBuilder<IWellknownLocationConfig> {
-	protected *buildLocationFile(status) {
+	protected * buildLocationFile(status) {
 		if (status.direction === RouteDirection.OUT) {
 			return;
 		}
 		
-		const root = `/data/certbot-root/${this.service.serviceName}/`;
+		const root = `/data/certbot-root/${this.service.serviceName}/.well-known/`;
 		
 		if (isDocker) {
 			ensureDirSync(root);
-			if (!pathExistsSync(root + 'index.html')) {
-				//language=TEXT
-				writeFileSync(root + 'index.html', `<h1>SSL request folder for service ${this.service.serviceName}</h1>`, {encoding: 'utf8'});
-			}
+			//language=TEXT
+			writeFileSync(root + 'index.html', `<h1>SSL request folder for service ${this.service.serviceName}</h1>`, {encoding: 'utf8'});
 		}
 		
 		yield new WellknownLocationConfigFile({
